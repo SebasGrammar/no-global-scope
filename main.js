e682584a47987fdf282926728dd9295f25df2ebd
Sebastian <sebasgrammar@hotmail.com>
2020-01-23 19:44:52 -0500

    Ways to avoid declaring variables in the global scope

---
+++ main.js
@@ -0,0 +1,75 @@
+const test = [1, 2, 3, 4, 5];
+
+// PRIVATE VARIABLES. AVOID USING GLOBAL CONTEXT, ETC.
+
+(function(array) {
+
+  // counter = 0; -> GLOBAL VARIABLE, WATCH OUT!
+  let counter = 0;
+
+  const up = document.querySelector(".up")
+  const down = document.querySelector(".down")
+
+  function uppie() {
+    counter ++
+    console.log(array[counter])
+  }
+
+  function downie() {
+    counter --
+    console.log(array[counter])
+  }
+
+  // 
+  up.addEventListener("click", uppie)
+  down.addEventListener("click", downie)
+
+})(test);
+//console.log(counter)
+// CLOSURES
+
+function returnFunction() {
+    let counter = 0;
+    return function() {
+        console.log(counter ++)
+    }
+}
+
+const countUp = returnFunction()
+
+countUp()
+countUp()
+countUp()
+countUp()
+
+// MODULE PATTERN BELOW
+
+const a = (function() {
+
+    let arr = [1, 2, 3]
+    let counter = arr[arr.length - 1]
+
+    return {
+        add() {
+            counter ++
+            arr.push(counter)
+            console.log(arr)
+        },
+        remove() {
+            counter --
+            arr.pop()
+            console.log(arr)
+        }
+    }
+
+})();
+
+a.add()
+a.add()
+a.add()
+
+a.remove()
+a.remove()
+a.remove()
+
+a.add()
